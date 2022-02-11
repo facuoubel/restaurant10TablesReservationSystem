33814188b274ddac1d4a7f50d1c783a98230102e
Facun <Facun@DESKTOP-R9A20NM>
2022-02-11 08:12:58 -0300

    Agregar archivos de proyecto.

---
+++ restaurant10TablesReservationSystem/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace restaurant10TablesReservationSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
