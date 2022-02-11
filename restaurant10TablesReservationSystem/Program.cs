aeaf162e3d7c52b10781c9ecb61a0d2adce92289
Facundo Oubel <FacundoOubel@hotmail.com>
2022-02-11 09:37:27 -0300

    cambio

--- restaurant10TablesReservationSystem/Program.cs
+++ restaurant10TablesReservationSystem/Program.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+/*
+ *1. Validar si el usuario existe o si se debe registrar; generar un sistema de registro o login.
+ *2. El programa debe ser capaz de darle la bienvenida a un usuario existente
+ *3. El programa debe repetirse hasta que se registren los diez comensales. 
+*/
 
 namespace restaurant10TablesReservationSystem
 {
@@ -6,7 +11,24 @@ namespace restaurant10TablesReservationSystem
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[] userNames = new string[10] { "Facu", "", "", "", "", "", "", "", "", "" };
+            int arrayCurrentIndex = 1;
+            bool userType;
+            Console.WriteLine("Welcome to the Facu restaurant!");
+         
+
+                while(true)
+            {
+                Console.WriteLine("Are you a registered user? Write true, or false to register");
+                userType = Convert.ToBoolean(Console.ReadLine());
+                if (userType == true)
+                {
+                    Console.WriteLine("Hello, please enter your user name:");
+                        string userToSearch = Console.ReadLine();
+                    Console.WriteLine("The user you searched is {0}", userToSearch);
+                }
+            }
+
         }
     }
 }
